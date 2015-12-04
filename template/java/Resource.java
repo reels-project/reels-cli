3434434a484f220b55cff183b146d3e7755f0914
Takahiko Sato <t.sato@mosa-architect.com>
2015-12-04 13:41:39 +0900

    検索機能を追加For Rest

--- template/java/Resource.java
+++ template/java/Resource.java
@@ -1,14 +1,20 @@
 package <%- packageName %>.resource;
 
 import java.net.URI;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Named;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
 import javax.transaction.Transactional;
 import javax.validation.Valid;
+import javax.ws.rs.BeanParam;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
@@ -17,23 +23,63 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-import <%- packageName %>.model.SampleModel;
+import <%- packageName %>.model.<%- targetName2 %>Model;
 
 @Path("<%- targetName %>")
 @Produces(MediaType.APPLICATION_JSON)
 @RequestScoped
 @Named
-public class Resource {
+public class <%- targetName2 %>Resource {
 	
 	@PersistenceContext
 	private EntityManager em;
-	
+
+	public static class <%- targetName2 %>Condition{
+		<% columns.forEach(function(c){ %>
+		@QueryParam("<%- c.name %>")
+		private <%- c.javaType %> <%- c.name %>;
+		<% }) %>
+		<% columns.forEach(function(c){ %>
+		public <%- c.javaType %> get<%- c.javaName %>() {
+			return <%- c.name %>;
+		}
+
+		public void set<%- c.javaName %>(<%- c.javaType %> <%- c.name %>) {
+			this.<%- c.name %> = <%- c.name %>;
+		}
+		<% }) %>
+	}
+
 	@GET
-	public List<<%- targetName2 %>Model> all(){
-		return em.createQuery("select s from <%- targetName2 %>Model s",<%- targetName2 %>Model.class).getResultList();
+	public List<<%- targetName2 %>Model> search(@BeanParam <%- targetName2 %>Condition condition){
+		Map<String,Object> params = new LinkedHashMap<>();
+		List<String> conditions = new ArrayList<>();
+
+		//量産型検索条件たち
+		<% columns.forEach(function(c){ %>
+		<% if(c.javaType === 'String'){ %>
+		if(condition.get<%-c.javaName%>() != null && !condition.get<%-c.javaName%>().isEmpty()){
+		<% }else{ %>
+		if(condition.get<%-c.javaName%>() != null){
+		<% } %>
+			conditions.add("s.<%-c.name%> = :<%-c.name%>");
+			params.put("<%-c.name%>", condition.get<%-c.javaName%>());
+		}
+		<% }) %>
+
+		String jpql = "select s from <%-targetName2%>Model s";
+		String where = conditions.stream().collect(Collectors.joining(" and "));
+		if(!where.isEmpty()){
+			jpql = jpql + " where " + where;
+		}
+
+		TypedQuery<<%-targetName2%>Model> q = em.createQuery(jpql,<%-targetName2%>Model.class);
+		params.entrySet().forEach(e -> q.setParameter(e.getKey(), e.getValue()));
+		return q.getResultList();
 	}
 	
 	@GET
